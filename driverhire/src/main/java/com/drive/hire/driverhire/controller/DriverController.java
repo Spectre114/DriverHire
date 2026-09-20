8837989bd6b3b65bbfd238c989ef134248c3a07a
Spectre114 <hmtv2003@gmail.com>
2026-09-20 20:48:43 +0530

    Add basic api for driver

---
+++ driverhire/src/main/java/com/drive/hire/driverhire/controller/DriverController.java
@@ -0,0 +1,10 @@
+package com.drive.hire.driverhire.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/driver/")
+public class DriverController {
+
+}
