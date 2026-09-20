8af45e1d2c1f8eee4ef8ec8fdc9f298b887c3a33
Spectre114 <hmtv2003@gmail.com>
2026-09-20 20:40:50 +0530

    Project start

---
+++ driverhire/src/main/java/com/drive/hire/driverhire/DriverhireApplication.java
@@ -0,0 +1,13 @@
+package com.drive.hire.driverhire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverhireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverhireApplication.class, args);
+	}
+
+}
---
+++ driverhire/src/test/java/com/drive/hire/driverhire/DriverhireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drive.hire.driverhire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverhireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
